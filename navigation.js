85efa82ed1fab26cfa7176acdf5ce91bd6ce1344
codeAlphaBravosite <help.factchannel@gmail.com>
2024-11-17 03:04:21 +0530

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,27 @@
+import { loadHomePage } from '../storyboard.js';
+import { loadEditorPage } from '../scene.js';
+import { loadPreviewPage } from '../preview.js';
+
+export function setupNavigation() {
+  window.addEventListener('hashchange', handleRoute);
+  handleRoute();
+}
+
+function handleRoute() {
+  const route = window.location.hash.slice(1);
+  const [page, id] = route.split('/');
+
+  switch (page) {
+    case 'home':
+      loadHomePage();
+      break;
+    case 'editor':
+      loadEditorPage(id);
+      break;
+    case 'preview':
+      loadPreviewPage(id);
+      break;
+    default:
+      loadHomePage();
+  }
+}
